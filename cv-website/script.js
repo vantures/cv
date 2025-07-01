bbd2ae4628637a9ff97b6a07457ecf6d851329fb
orangehotwheel <cvan84@gmail.com>
2025-07-01 10:43:27 -0400

    Flatten folder structure (remove extra cv-website directory)

--- cv-website/script.js
+++
@@ -1,7 +0,0 @@
-// Set current year in footer
-(function () {
-  const yearSpan = document.getElementById('year');
-  if (yearSpan) {
-    yearSpan.textContent = new Date().getFullYear();
-  }
-})(); 
